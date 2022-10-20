d968802107607cde1e686e7f0c9cdb615949302f
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-10-20 17:22:01 +0500

    added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
 import Layout from '../components/layout/Layout'
+import NextNProgress from "nextjs-progressbar";
 // css
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
+       <>
+          <NextNProgress
+        color="#e6007aFF"
+        startPosition={0.1}
+        stopDelayMs={100}
+        height={3}
+        showOnShallow={true}
+      />
     <Layout>
       <Component {...pageProps} />
     </Layout>
+       </>
   )
 }
 
